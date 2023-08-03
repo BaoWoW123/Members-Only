5b477a0c122c6b38ac617fea0d65bf46eef893eb
BaoWoW <baonguyen9392@gmail.com>
2023-08-03 13:44:47 -0700

    Update Post Signup Method Error Handling
    Removed previous validation code.
    Updated to custom validators with express-validator for sign up form.

--- Members-Only/routes/index.js
+++ Members-Only/routes/index.js
@@ -31,22 +31,30 @@ router.post(
       .trim()
       .isLength({ min: 5 })
       .escape(),
+      check("confirmPassword", "Confirmed password must be at least 5 characters long")
+      .trim()
+      .isLength({ min: 5 })
+      .escape(),
     check("username", "Username must be 5-20 characters")
       .trim()
       .isLength({ min: 5, max: 20 })
       .escape(),
+    check('username').custom(async (username)=> {
+        let usernameTaken = await User.findOne({ username: username });
+        if (usernameTaken) {
+            throw new Error('Username taken')
+        }
+    }),
+    check('confirmPassword').custom( (password, {req}) => {
+        if (password !== req.body.password) {
+            throw new Error(`Passwords do not match`)
+        } else return true;
+    })
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.render("signup", { errors: errors.array() });
-    let usernameTaken = await User.findOne({ username: req.body.username });
-    if (usernameTaken)
-      return res.render("signup", { errors: [{ msg: "Username taken" }] });
-    if (req.body.password !== req.body.confirmPassword)
-      return res.render("signup", {
-        errors: [{ msg: "Passwords do not match" }],
-      });
     try {
       bcrypt.hash(req.body.password, 10, async (err, hashedPw) => {
         if (err) return next(err);
